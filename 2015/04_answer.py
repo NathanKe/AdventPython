0b1e505c7c57ff7a0225dbcfd80b8038e3e9af7e
Nathan Kearney <nkearney89@gmail.com>
2019-11-26 22:11:58 -0500

    15_04 complete

---
+++ 2015/04_answer.py
@@ -0,0 +1,27 @@
+import hashlib
+
+
+def md5_first_n(instr, n):
+    return hashlib.md5(instr.encode()).hexdigest()[0:n]
+
+
+puzzleStr = 'iwrupvqb'
+
+i = 0
+while True:
+    md5 = md5_first_n(puzzleStr+str(i), 5)
+    if md5 == '00000':
+        break
+    else:
+        i += 1
+
+print('Part 1:', i)
+
+while True:
+    md5 = md5_first_n(puzzleStr+str(i), 6)
+    if md5 == '000000':
+        break
+    else:
+        i += 1
+
+print('Part 2:', i)
