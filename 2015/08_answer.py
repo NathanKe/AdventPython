6dfd19e141ba21f8dd3376b17fff3dcd331eeac0
Nathan Kearney <nkearney89@gmail.com>
2019-11-27 18:10:26 -0500

    15_08 complete

---
+++ 2015/08_answer.py
@@ -0,0 +1,25 @@
+import re
+
+lines = open('08_input').read().splitlines()
+
+total_len = sum(map(len, lines))
+
+compress_len = sum(map(len, list(map(eval, lines))))
+
+
+def encode(str):
+    result = ''
+    for ch in str:
+        if ch == '"':
+            result += '\\\"'
+        elif ch == '\\':
+            result += '\\\\'
+        else:
+            result += ch
+    return '"'+result+'"'
+
+
+expand_len = sum(map(len, list(map(encode, lines))))
+
+print('Part 1:', total_len - compress_len)
+print('Part 2:', expand_len - total_len)
