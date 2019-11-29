53fb896f90550697b347936c1cb86366e132d1bd
Nathan Kearney <nkearney89@gmail.com>
2019-11-29 14:30:43 -0500

    15_09 complete: speed up not checking each perm both directions

--- 2015/09_answer.py
+++ 2015/09_answer.py
@@ -8,11 +8,13 @@ locs = list(set(list(map(lambda s: re.sub(r"(\S+)(\s.+)", r"\1", s), lines))))
 locs.append(lines[-1].split()[2])
 
 loc_perm = list(itertools.permutations(locs))
+half_len = len(loc_perm)//2
+half_sorted_perm = sorted(loc_perm)[:half_len]
 
 cur_min = 1000000
 cur_max = 0
 
-for path in loc_perm:
+for path in half_sorted_perm:
     steps = [(path[i], path[i+1]) for i in range(0, len(path) - 1)]
     dist = 0
     for step in steps:
