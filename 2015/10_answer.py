10fc5ad151f8c9ff4505e8537044f69b706d5baa
Nathan Kearney <nkearney89@gmail.com>
2019-11-28 22:09:48 -0500

    15_10 complete

---
+++ 2015/10_answer.py
@@ -0,0 +1,27 @@
+import itertools
+
+sx = "3113322113"
+
+
+def look_say_expand(string):
+    grs = []
+    for _, g in itertools.groupby(string):
+        grs.append(''.join(g))
+    ret = ""
+    for g in grs:
+        ret += str(len(g))
+        ret += g[0]
+    return ret
+
+
+for i in range(40):
+    sx = look_say_expand(sx)
+
+
+print('Part 1: ', len(sx))
+
+for i in range(10):
+    sx = look_say_expand(sx)
+
+
+print('Part 2: ', len(sx))
