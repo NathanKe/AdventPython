f17918f229ffecd38e04aa807c6219af35f1aaaf
Nathan Kearney <nkearney89@gmail.com>
2019-12-01 17:09:24 -0500

    15_20 complete

--- 2015/20_answer.py
+++ 2015/20_answer.py
@@ -1,18 +1,38 @@
 import itertools
+import math
 
 target = 36000000
 
 
-def presents_by_elf(elf):
-    x = [0 for i in range(elf - 1)]
-    x.append(10 * elf)
-    return x * 50
+def divisors(n):
+    divs = []
+    for i in range(1, math.floor(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divs.append(i)
+            if n // i != i:
+                divs.append(n // i)
+    return divs
 
 
-masks = [presents_by_elf(i) for i in range(1000)]
+def present_count_1(dvs):
+    return sum(map(lambda d: d * 10, dvs))
 
-zipped = itertools.zip_longest(*masks, fillvalue=0)
 
-summed = map(sum, zipped)
+def present_count_2(house, dvs):
+    return sum(map(lambda d: d * 11, filter(lambda di: house // di <= 50, dvs)))
 
-# first_greater_than_target = list(filter(lambda i: i >= target, summed))[0]
+
+p1, p2 = 0, 0
+for h in itertools.count(800000):
+    dvs = divisors(h)
+    if p1 == 0 and present_count_1(dvs) >= target:
+        p1 = h
+    if p2 == 0 and present_count_2(h, dvs) >= target:
+        p2 = h
+    if p1 != 0 and p2 != 0:
+        break
+    if h % 10000 == 0:
+        print(h)
+
+print('Part 1: ', p1)
+print('Part 2: ', p2)
