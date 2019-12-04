ec8ab505c07146794be5af73c362601eac8f7896
Nathan Kearney <nkearney89@gmail.com>
2019-12-03 22:07:00 -0500

    16_07 complete

--- 2019/07_answer.py
+++ 2019/07_answer.py
@@ -39,3 +39,43 @@ def passes_abba_check(chck):
 
 
 print('Part 1: ', len(list(filter(passes_abba_check, to_check))))
+
+
+def distinct_aba_string(s):
+    return list(set(filter(lambda x: x[0] != x[1], map(lambda tu: tu[0], re.findall(r"(?=((.).\2))", s)))))
+
+
+def distinct_aba_string_list(li):
+    all_res = []
+    for item in li:
+        cur_res = distinct_aba_string(item)
+        for aba in cur_res:
+            all_res.append(aba)
+    return list(set(all_res))
+
+
+def aba_out(s):
+    split = re.split(r"[\[\]]", s)
+    outs = split[0::2]
+    return distinct_aba_string_list(outs)
+
+
+def aba_in(s):
+    split = re.split(r"[\[\]]", s)
+    ins = split[1::2]
+    return distinct_aba_string_list(ins)
+
+
+def aba_out_and_bab_in(s):
+    outs = aba_out(s)
+    ins = aba_in(s)
+
+    outs_rev = [a[1] + a[0] + a[1] for a in outs]
+
+    for rev in outs_rev:
+        if rev in ins:
+            return True
+    return False
+
+
+print('Part 2: ', len(list(filter(aba_out_and_bab_in, to_check))))
