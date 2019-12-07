5ae98352264bcc4f0c0a722c247aa79d773c2fb9
Nathan Kearney <nkearney89@gmail.com>
2019-12-06 22:59:38 -0500

    16_11 in progress

--- 2016/11_answer.py
+++ 2016/11_answer.py
@@ -1,10 +1,18 @@
-import re
 import itertools
+import collections
+import copy
 
 puzzle_state_1 = [
     ["EEE", "ThG", "ThM", "PlG", "StG"],
     ["PlM", "StM"],
     ["PrG", "PrM", "RuG", "RuM"],
+    []
+]
+
+goal_state_1 = [
+    ["EEE", "P1G", "PlM", "PrG", "PrM", "RuG", "RuM", "StG", "StM", "ThG", "ThM"],
+    [],
+    [],
     []
 ]
 
@@ -34,14 +42,24 @@ def is_safe_state(p_state):
     return True
 
 
-def carry_up(p_state, p_carry):
-    # return new state
-    ...
+def carry_up(p_state, p_carry, p_floor):
+    out_state = copy.deepcopy(p_state)
+    for item in p_carry:
+        out_state[p_floor].remove(item)
+        out_state[p_floor + 1].append(item)
+    out_state[p_floor + 1].append('EEE')
+    out_state[p_floor].remove('EEE')
+    return out_state
 
 
-def carry_down(p_state, p_carry):
-    # return new state
-    ...
+def carry_down(p_state, p_carry, p_floor):
+    out_state = copy.deepcopy(p_state)
+    for item in p_carry:
+        out_state[p_floor].remove(item)
+        out_state[p_floor - 1].append(item)
+    out_state[p_floor - 1].append('EEE')
+    out_state[p_floor].remove('EEE')
+    return out_state
 
 
 def adjacent_states(p_state):
@@ -51,19 +69,54 @@ def adjacent_states(p_state):
     elev_floor = list(map(lambda fl: "EEE" in fl, p_state)).index(True)
     if elev_floor > 0:
         avail_dirs.append("DOWN")
-    if elev_floor < len(p_state):
+    if elev_floor < len(p_state) - 1:
         avail_dirs.append("UP")
 
-    poss_items = p_state[elev_floor]
+    poss_items = p_state[elev_floor][:]
     poss_items.remove('EEE')
     poss_carry = list(itertools.combinations(poss_items, 1)) + list(itertools.combinations(poss_items, 2))
     for poss_action in list(itertools.product(avail_dirs, poss_carry)):
         if poss_action[0] == 'UP':
-            new_state = carry_up()
+            new_state = carry_up(p_state, poss_action[1], elev_floor)
             if is_safe_state(new_state):
                 result_states.append(new_state)
         if poss_action[0] == 'DOWN':
-            new_state = carry_down()
-            if is_safe_state(new_state):
-                result_states.append(new_state)
+            if len(p_state[elev_floor - 1]) != 0:
+                new_state = carry_down(p_state, poss_action[1], elev_floor)
+                if is_safe_state(new_state):
+                    result_states.append(new_state)
     return result_states
+
+
+frontier = collections.deque([(puzzle_state_1, [])])
+solved = {}
+
+
+def state_to_string(p_state):
+    out = ""
+    for i in range(len(p_state)):
+        fl = "Floor %d:" % (i + 1)
+        items = ""
+        for item in sorted(p_state[i]):
+            items += item
+            items += ","
+        fl += items
+        out += fl + "\n"
+    out += "########\n"
+    return out
+
+
+goal_string = state_to_string(goal_state_1)
+
+i = 0
+while len(frontier) > 0 and i < 10000:
+    cur_state, cur_path = frontier.pop()
+    for adj in adjacent_states(cur_state):
+        if state_to_string(adj) not in solved.keys():
+            frontier.append((adj, cur_path + [adj]))
+    cur_string = state_to_string(cur_state)
+    solved[cur_string] = cur_path
+    if cur_string == goal_string:
+        print('GOAL!')
+        break
+    i += 1
