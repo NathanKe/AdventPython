24ccbc71dca18048248c6568dcffd499284412b9
Nathan Kearney <kearneyn@brynmawrschool.org>
2019-12-09 14:34:10 -0500

    19_09 refined

--- 2019/09_answer.py
+++ 2019/09_answer.py
@@ -1,4 +1,5 @@
 import operator
+import collections
 
 puzzle = [1102, 34463338, 34463338, 63, 1007, 63, 34463338, 63, 1005, 63, 53, 1101, 0, 3, 1000, 109, 988, 209, 12, 9,
           1000, 209, 6, 209, 3, 203, 0, 1008, 1000, 1, 63, 1005, 63, 65, 1008, 1000, 2, 63, 1005, 63, 904, 1008, 1000,
@@ -43,16 +44,10 @@ puzzle = [1102, 34463338, 34463338, 63, 1007, 63, 34463338, 63, 1005, 63, 53, 11
 
 
 class IntCode:
-    code = None
-    indx = None
-    outs = None
-    inps = None
-    inp_ind = None
-    halt = None
-    rel_base = None
-
     def __init__(self, inps, init_code):
-        self.code = init_code[:] + [0] * 10000
+        self.code = collections.defaultdict(int)
+        for i in range(len(init_code)):
+            self.code[i] = init_code[i]
         self.indx = 0
         self.outs = []
         self.inps = inps
@@ -60,31 +55,9 @@ class IntCode:
         self.halt = False
         self.rel_base = 0
 
-    def run_instr(self):
-        cmd = self.code[self.indx] % 100
-        if cmd == 1:
-            self.ex_combine(operator.add)
-        elif cmd == 2:
-            self.ex_combine(operator.mul)
-        elif cmd == 3:
-            self.ex_put_input()
-        elif cmd == 4:
-            self.ex_write_out()
-        elif cmd == 5:
-            self.ex_jump_if(operator.truth)
-        elif cmd == 6:
-            self.ex_jump_if(operator.not_)
-        elif cmd == 7:
-            self.ex_compare(operator.lt)
-        elif cmd == 8:
-            self.ex_compare(operator.eq)
-        elif cmd == 9:
-            self.ex_rel_base_adj()
-        elif cmd == 99:
-            self.ex_halt()
-
     def get_nth_param(self, n):
         mode = (self.code[self.indx] // 10 ** (1 + n)) % 10
+        #              0 = position              1 = immediate  2 = relative
         base_params = [self.code[self.indx + n], self.indx + n, self.code[self.indx + n] + self.rel_base]
         param = base_params[mode]
         return param
@@ -103,6 +76,30 @@ class IntCode:
             param3 = None
         return param1, param2, param3
 
+    def run_instr(self):
+        cmd = self.code[self.indx] % 100
+        param1, param2, param3 = self.get_n_params(3)
+        if cmd == 1:
+            self.ex_combine(operator.add, param1, param2, param3)
+        elif cmd == 2:
+            self.ex_combine(operator.mul, param1, param2, param3)
+        elif cmd == 3:
+            self.ex_put_input(param1)
+        elif cmd == 4:
+            self.ex_write_out(param1)
+        elif cmd == 5:
+            self.ex_jump_if(operator.truth, param1, param2)
+        elif cmd == 6:
+            self.ex_jump_if(operator.not_, param1, param2)
+        elif cmd == 7:
+            self.ex_compare(operator.lt, param1, param2, param3)
+        elif cmd == 8:
+            self.ex_compare(operator.eq, param1, param2, param3)
+        elif cmd == 9:
+            self.ex_rel_base_adj(param1)
+        elif cmd == 99:
+            self.ex_halt()
+
     def run_code_to_halt(self):
         while not self.halt:
             self.run_instr()
@@ -117,40 +114,32 @@ class IntCode:
         self.run_code_to_next_out_or_halt()
         return self.outs[-1]
 
-    def ex_rel_base_adj(self):
-        param1, _, _ = self.get_n_params(1)
-        self.rel_base += self.code[param1]
-        self.indx += 2
-
-    def ex_combine(self, op):
-        param1, param2, param3 = self.get_n_params(3)
+    def ex_combine(self, op, param1, param2, param3):
         self.code[param3] = op(self.code[param1], self.code[param2])
         self.indx += 4
 
-    def ex_put_input(self):
-        print(self.inps)
-        param1, _, _ = self.get_n_params(1)
+    def ex_put_input(self, param1):
         self.code[param1] = self.inps[self.inp_ind]
         self.inp_ind += 1
         self.indx += 2
 
-    def ex_write_out(self):
-        param1, _, _ = self.get_n_params(1)
-        # assert sum(self.outs) == 0
+    def ex_write_out(self, param1):
         self.outs.append(self.code[param1])
         self.indx += 2
 
-    def ex_jump_if(self, op):
-        param1, param2, _ = self.get_n_params(2)
+    def ex_jump_if(self, op, param1, param2):
         if op(self.code[param1]):
             self.indx = self.code[param2]
         else:
             self.indx += 3
 
-    def ex_compare(self, op):
-        param1, param2, param3 = self.get_n_params(3)
+    def ex_compare(self, op, param1, param2, param3):
         self.code[param3] = int(op(self.code[param1], self.code[param2]))
         self.indx += 4
+
+    def ex_rel_base_adj(self, param1):
+        self.rel_base += self.code[param1]
+        self.indx += 2
 
     def ex_halt(self):
         self.halt = True
@@ -158,16 +147,8 @@ class IntCode:
 
 def code_runner(p_inputs, p_code):
     int_code = IntCode(p_inputs, p_code)
-    int_code.run_code_to_halt()
-    return int_code.outs
+    return int_code.run_code_to_halt()
 
-
-t_1 = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
-t_2 = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
-t_3 = [104, 1125899906842624, 99]
 
 print('Part 1: ', code_runner([1], puzzle))
 print('Part 2: ', code_runner([2], puzzle))
-
-
-
