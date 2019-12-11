3e890ddb8d258bd151ecfc9f618e1a2987c13e8c
Nathan Kearney <nkearney89@gmail.com>
2019-12-10 22:35:15 -0500

    19_10 complete

--- 2019/10_answer.py
+++ 2019/10_answer.py
@@ -28,7 +28,7 @@ def vis_asteroids_from_point(p_grid, s_x, s_y):
     for x in p_grid.keys():
         for y in p_grid[x].keys():
             cl_x, cl_y = classify_point(x - s_x, y - s_y)
-            if p_grid[x][y]:
+            if (cl_x or cl_y) and p_grid[x][y]:
                 class_dict[(cl_x, cl_y)].append((x, y))
     return class_dict
 
@@ -40,34 +40,37 @@ def max_vis_asteroid(p_grid):
             if p_grid[x][y]:
                 c_vis = vis_asteroids_from_point(p_grid, x, y)
                 if len(c_vis) > len(c_max):
-                    c_max = c_vis.copy()
+                    c_max = c_vis
     return c_max
 
 
-best_station = max_vis_asteroid(generate_dict(teninput.t_4))
+best_station = max_vis_asteroid(generate_dict(teninput.puzzle))
 print('Part 1: ', len(best_station))
 
 
 def convert_to_degrees_clockwise_from_north(p_station):
     out = []
     for k in p_station.keys():
-        deg = (-1 * math.atan2(k[1], k[0]) * 180 / math.pi + 90 + 360) % 360
-        out.append((deg, p_station[k]))
+        # k[1] is y, k[0] is x
+        # negate y to account for up positive on Cartesian, up is descending in grid
+        # negate atan2 to run CW
+        # 180/pi to get degrees
+        # add 90 to move to 0 = Y Axis
+        # add 360 to constrain as positive
+        # mod 360 to constrain [0, 360]
+        deg = (-1 * math.atan2(-1 * k[1], k[0]) * 180 / math.pi + 90 + 360) % 360
+        out.append((deg, k, p_station[k]))
     out.sort()
     return out
 
 
-target_deque = collections.deque(convert_to_degrees_clockwise_from_north(max_vis_asteroid(generate_dict(teninput.t_4))))
+target_deque = collections.deque(convert_to_degrees_clockwise_from_north(best_station))
 eliminated = []
 
 while len(eliminated) < 200:
-    c_angle, c_ast_deque = target_deque.pop()
-    print(c_angle, c_ast_deque)
+    c_angle, c_loc, c_ast_deque = target_deque.popleft()
     eliminated.append(c_ast_deque.pop())
     if len(c_ast_deque) > 0:
-        print(c_angle, c_ast_deque)
-        target_deque.appendleft((c_angle, c_ast_deque))
+        target_deque.append((c_angle, c_loc, c_ast_deque))
 
-print('Part 2: ', eliminated[-1])
-
-
+print('Part 2: ', eliminated[-1][0] * 100 + eliminated[-1][1])
