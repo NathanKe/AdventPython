a5195ca21db8cdb66b7c13e45f01b306c9f38dcf
Nathan Kearney <nkearney89@gmail.com>
2021-12-15 19:39:55 -0500

    21 14 a&b w/ help from jonathanpaulson

--- 2021/14_answer.py
+++ 2021/14_answer.py
@@ -14,56 +14,32 @@ for rule in rules:
     rule_hash[lft] = rgt
 
 
-def init_deque(in_str):
-    out_deque = deque()
-    for i in range(len(in_str)):
-        out_deque.append(in_str[i])
-    return out_deque
+def pair_count(in_template, depth):
+    c_a = Counter()
+    for i in range(len(in_template) - 1):
+        c_a[in_template[i] + in_template[i + 1]] += 1
+
+    for i in range(depth):
+        c_b = Counter()
+        for p in c_a:
+            res = rule_hash[p]
+            c_b[p[0] + res] += c_a[p]
+            c_b[res + p[1]] += c_a[p]
+        c_a = c_b
+    return c_a
 
 
-def expand(polymer):
-    p_dq = deque(polymer)
-    new_dq = deque()
-    p_1 = p_dq.popleft()
-    while p_dq:
-        p_2 = p_dq.popleft()
-        new_dq.append(p_1)
-        new_dq.append(rule_hash[p_1 + p_2])
-        p_1 = p_2
-    new_dq.append(p_2)
-    return ''.join(new_dq)
+def most_minus_least(in_template, depth):
+    letter_counter = Counter()
+    letter_counter[in_template[-1]] += 1
+    pair_counter = pair_count(in_template, depth)
+
+    for pair, count in pair_counter.items():
+        letter_counter[pair[0]] += count
+
+    ord_count = letter_counter.most_common()
+    return ord_count[0][1] - ord_count[-1][1]
 
 
-def count_recur(polymer, exp_count):
-    print(polymer)
-    if exp_count > 0:
-        if len(polymer) >= 2:
-            polymer = expand(polymer)
-    p_len = len(polymer)
-    if p_len == 2:
-        if polymer in rule_hash.keys():
-            return Counter(list(polymer)) + Counter(list(rule_hash[polymer]))
-        else:
-            return Counter(list(polymer))
-    elif p_len == 1:
-        return Counter(list(polymer))
-    else:
-        left = polymer[:p_len//2]
-        right = polymer[p_len//2:]
-        middle = rule_hash[polymer[p_len//2]+polymer[p_len//2 + 1]]
-        l_c = Counter(count_recur(left, exp_count - 1))
-        r_c = Counter(count_recur(right, exp_count - 1))
-        m_c = Counter(middle)
-        return l_c + r_c + m_c
-
-
-def most_minus_least(ct):
-    c_ord = ct.most_common()
-    most = c_ord[0][1]
-    least = c_ord[-1][1]
-    return most - least
-
-
-print("Part 1: ", most_minus_least(count_recur(template, 10)))
-
-
+print("Part 1: ", most_minus_least(template, 10))
+print("Part 2: ", most_minus_least(template, 40))
