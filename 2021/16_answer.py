676f7a1011c8f0b5ff526f45bf52363f43a49668
Nathan Kearney <nkearney89@gmail.com>
2021-12-17 17:45:07 -0500

    21 16 b

--- 2021/16_answer.py
+++ 2021/16_answer.py
@@ -1,3 +1,6 @@
+from collections import deque
+from numpy import prod
+
 hex_string = open('16_input').read().splitlines()[0]
 
 
@@ -6,15 +9,10 @@ def hex_char_bin_four(ch):
 
 
 def hex_translate(h):
-    return ''.join(map(hex_char_bin_four, list(h)))
+    return deque(''.join(map(hex_char_bin_four, list(h))))
 
 
-def function_apply(f_type_bin, sub_vals):
-    f_type = int(f_type_bin, 2)
-
-    if type(sub_vals) == int:
-        sub_vals = [sub_vals]
-
+def function_apply(f_type, sub_vals):
     res = None
     if f_type == 0:
         res = 0
@@ -46,57 +44,49 @@ def function_apply(f_type_bin, sub_vals):
     return res
 
 
-def parse(bin_packet, version_sum, packet_result):
-    print(bin_packet, version_sum, packet_result)
-    if '1' not in bin_packet:
-        return '', version_sum, packet_result
-    else:
-        v = bin_packet[0:3]
-        version_sum += int(v, 2)
-        t = bin_packet[3:6]
-        if t == '100':
-            prefix_index = 6
-            literal_val = 0
-            while bin_packet[prefix_index] == '1':
-                literal_val *= 10
-                literal_val += int(bin_packet[prefix_index + 1: prefix_index + 5], 2)
-                prefix_index += 5
-            literal_val += int(bin_packet[prefix_index + 1: prefix_index + 5])
-            bin_packet = bin_packet[prefix_index + 5:]
-            packet_result = literal_val
-            bin_packet, version_sum, packet_result = parse(bin_packet, version_sum, packet_result)
-        else:
-            l_i = bin_packet[6]
-            # length defined
-            if l_i == '0':
-                sub_packet_length_bit = bin_packet[7:22]
-                sub_packet_length_int = int(sub_packet_length_bit, 2)
-                sub_packet_data = bin_packet[22:22 + sub_packet_length_int]
-                sub_sum = 0
-                sub_packet_results = []
-                while sub_packet_data:
-                    sub_packet_data, sub_sum, x_res = parse(sub_packet_data, sub_sum, packet_result)
-                    sub_packet_results.append(x_res)
-                version_sum += sub_sum
-                sub_res = [function_apply(t, e) for e in sub_packet_results]
-                packet_result = function_apply(t, [packet_result, sub_res])
-                bin_packet = bin_packet[22 + sub_packet_length_int:]
-                bin_packet, version_sum, packet_result = parse(bin_packet, version_sum, packet_result)
-            # count defined
-            elif l_i == '1':
-                sub_packet_count_bit = bin_packet[7:18]
-                sub_packet_count_int = int(sub_packet_count_bit, 2)
-                bin_packet = bin_packet[18:]
-                sub_packet_results = []
-                for i in range(sub_packet_count_int):
-                    bin_packet, version_sum, x_res = parse(bin_packet, version_sum, packet_result)
-                    sub_packet_results.append(x_res)
-                sub_res = [function_apply(t, e) for e in sub_packet_results]
-                packet_result = function_apply(t, [packet_result, sub_res])
-                bin_packet, version_sum, packet_result = parse(bin_packet, version_sum, packet_result)
-        return bin_packet, version_sum, packet_result
+def parse(packet, version_sum):
+    # zero tail case
+    if '1' not in packet:
+        return '', version_sum, 0
+
+    version = int(''.join([packet.popleft(), packet.popleft(), packet.popleft()]), 2)
+    version_sum += version
+    type_id = int(''.join([packet.popleft(), packet.popleft(), packet.popleft()]), 2)
+
+    # literal case
+    if type_id == 4:
+        prefix_index = packet.popleft()
+        literal_bin_string = ''
+        while prefix_index == '1':
+            literal_bin_string += ''.join([packet.popleft(), packet.popleft(), packet.popleft(), packet.popleft()])
+            prefix_index = packet.popleft()
+        literal_bin_string += ''.join([packet.popleft(), packet.popleft(), packet.popleft(), packet.popleft()])
+        literal_int = int(literal_bin_string, 2)
+        return packet, version_sum, literal_int
+
+    # get length type
+    sub_packet_type = packet.popleft()
+
+    # length case
+    if sub_packet_type == '0':
+        sub_packet_length = int(''.join([packet.popleft() for _ in range(15)]), 2)
+        sub_packet = deque(''.join([packet.popleft() for _ in range(sub_packet_length)]))
+        sub_vals = []
+        while sub_packet:
+            _, version_sum, sub_res = parse(sub_packet, version_sum)
+            sub_vals.append(sub_res)
+        return packet, version_sum, function_apply(type_id, sub_vals)
+
+    # count case
+    if sub_packet_type == '1':
+        sub_packet_count = int(''.join([packet.popleft() for _ in range(11)]), 2)
+        sub_vals = []
+        for i in range(sub_packet_count):
+            packet, version_sum, sub_res = parse(packet, version_sum)
+            sub_vals.append(sub_res)
+        return packet, version_sum, function_apply(type_id, sub_vals)
 
 
-a = parse(hex_translate(hex_string), 0, 0)
-print("Part 1: ", a)
-#print("Part 2: ", res)
+_, one, two = parse(hex_translate(hex_string), 0)
+print("Part 1: ", one)
+print("Part 2: ", two)
