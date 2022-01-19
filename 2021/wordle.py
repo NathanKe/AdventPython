a2a67fccbca153cfc47d2e23c5c9ae28e45ef6c4
NathanKe <nkearney89@gmail.com>
2022-01-19 16:17:21 -0500

    wordle stuff

--- 2021/wordle.py
+++ 2021/wordle.py
@@ -1,27 +1,40 @@
 # https://www.devangthakkar.com/wordle_archive/
 
 from collections import Counter
-import numpy as np
 
 data_word_list = open('word_list.txt').read().splitlines()
-
-no_repeats_word_list = list(filter(lambda w: Counter(w).most_common()[0][1] == 1, data_word_list))
+possible_results = open('possible_results.txt').read().splitlines()
 
 
 # 0 wrong, 1 right letter wrong place, 2 right letter in right place
 def reduce_by_guess_result(guess, encoding, word_list):
+    dupe_letters = []
+    for i in range(5):
+        sub_str = guess[:i]+guess[i+1:]
+        if guess[i] in sub_str:
+            dupe_letters.append(guess[i])
+
     for i, (l, c) in enumerate(zip(guess, encoding)):
         if c == '2':
             word_list = list(filter(lambda word: word[i] == l, word_list))
         elif c == '1':
             word_list = list(filter(lambda word: word[i] != l and l in word, word_list))
         elif c == '0':
-            word_list = list(filter(lambda word: l not in word, word_list))
+            if l in dupe_letters:
+                word_list = list(filter(lambda word: word[i] != l, word_list))
+            elif l not in dupe_letters:
+                word_list = list(filter(lambda word: l not in word, word_list))
 
     return word_list
 
 
-possible_results = open('possible_results.txt').read().splitlines()
+def get_largest_remaining_group(word, word_list):
+    largest_remaining_group = 0
+    for res in possible_results:
+        remainder_size = len(reduce_by_guess_result(word, res, word_list))
+        if remainder_size > largest_remaining_group:
+            largest_remaining_group = remainder_size
+    return largest_remaining_group
 
 
 def most_reductive_word(word_list):
@@ -44,57 +57,9 @@ def most_reductive_word(word_list):
     return best_word
 
 
-def largest_remaining_group(word, word_list, limiter = float('inf')):
-    largest_rem_size = 0
-    for res in possible_results:
-        rem_size = len(reduce_by_guess_result(word, res, word_list))
-        if rem_size > limiter:
-            return limiter
-        elif rem_size > largest_rem_size:
-            largest_rem_size = rem_size
-    return rem_size
-
-
-def depth_2_largest_group(word_1, word_2, word_list, limiter=float('inf')):
-    best_res = float('inf')
-    for res_1 in possible_results:
-        redux_1_list = reduce_by_guess_result(word_1, res_1, word_list)
-        cur_res = largest_remaining_group(word_2, redux_1_list, best_res)
-        if cur_res < best_res:
-            best_res = cur_res
-    return best_res
-
-
-def starting_pair(word_list):
-    smallest_largest = float('inf')
-    best = ('xxxxx', 'yyyyy')
-    for word_1 in word_list:
-        for res_1 in possible_results:
-            redux_1_list = reduce_by_guess_result(word_1, res_1, word_list)
-            for word_2 in word_list:
-                w2_fails = False
-                if all([w1 not in word_2 for w1 in word_1]):
-                    print(word_1, word_2, best, smallest_largest)
-                    depth_2_largest = 0
-                    for res_2 in possible_results:
-                        redux_2_list = reduce_by_guess_result(word_2, res_2, redux_1_list)
-                        cur_rem_len = len(redux_2_list)
-                        if cur_rem_len > smallest_largest:
-                            w2_fails = True
-                            break
-                        elif cur_rem_len > depth_2_largest:
-                            depth_2_largest = cur_rem_len
-                    if depth_2_largest < smallest_largest:
-                        smallest_largest = depth_2_largest
-                        best = (word_1, word_2)
-                if w2_fails:
-                    break
-
-
-
 def play():
     game_list = data_word_list
-    print("First Guess 'serai' as established best first word")
+    print("First Guess 'serai' precomputed as best first word")
     guess = 'serai'
     for i in range(6):
         result = input()
@@ -102,11 +67,11 @@ def play():
         guess = most_reductive_word(game_list)
         rem_len = len(game_list)
         info = f"Remaining words: {rem_len}, please guess: {guess}"
-        if rem_len < 15:
-            info = f"Remaining words: {game_list}"
+        if 2 <= rem_len < 15:
+            info = f"Remaining words: {game_list}, please guess: {guess}"
+        elif rem_len == 1:
+            info = f"Solution is: {guess}"
         print(info)
-        if rem_len < 6 - i:
-            break
 
 
 def free_play():
