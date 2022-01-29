3770b14a03e0dfc5513aa91968de044f64821663
Nathan Kearney <nkearney89@gmail.com>
2022-01-29 00:23:41 -0500

    wordle stuff

--- Wordle/best_start_search.py
+++ Wordle/best_start_search.py
@@ -54,21 +54,10 @@ def most_reductive_word(guessable_words, word_list, limit=15000):
     return best_word, smallest_largest_remaining_group
 
 
-def verbose_print(verbose, to_print):
-    if verbose:
-        print(to_print)
-
-
 def checker(guess, answer):
-    dupe_letters = []
-    for i in range(5):
-        sub_str = guess[:i]+guess[i+1:]
-        if guess[i] in sub_str:
-            dupe_letters.append(guess[i])
-
     result = ""
     for i in range(5):
-        if guess[i] in dupe_letters:
+        if guess[i] in guess[:i] + guess[i + 1:]:
             if guess[i] == answer[i]:
                 result += "2"
             elif guess[i] in answer[i:]:
@@ -82,7 +71,6 @@ def checker(guess, answer):
                 result += "1"
             else:
                 result += "0"
-
     return result
 
 
@@ -91,25 +79,25 @@ def retrieve_guess(game_list):
     if poss_redux == 1:
         guess = guess_from_poss
     else:
-        guess_from_all, all_redux = most_reductive_word(data_word_list, game_list, poss_redux)
-        if poss_redux <= all_redux:
+        guess_from_rem, rem_redux = most_reductive_word([d for d in data_word_list if d not in game_list], game_list,
+                                                        poss_redux)
+        if poss_redux <= rem_redux:
             guess = guess_from_poss
         else:
-            guess = guess_from_all
+            guess = guess_from_rem
     return guess
 
 
-big_fucking_hash = eval(open('big_fucking_hash_cache.txt').read().splitlines()[0])
+big_fucking_hash = {}
 
 
-# big_fucking_hash = {}
+# big_fucking_hash = eval(open('big_fucking_hash_cache.txt').read().splitlines()[0])
 
 
-def auto_play(answer, verbose=False):
-    verbose_print(verbose, f"{answer} -----")
+def auto_play(answer, start_guess):
     steps = 1
     game_list = answer_list
-    guess = 'arise'
+    guess = start_guess
 
     game_chain = guess
 
@@ -117,7 +105,6 @@ def auto_play(answer, verbose=False):
         result = checker(guess, answer)
         game_chain += result
         game_list = reduce_by_guess_result(guess, result, game_list)
-        verbose_print(verbose, f"{guess} : {result} : {len(game_list)}")
 
         if result == "22222":
             big_fucking_hash[game_chain] = guess
@@ -135,68 +122,37 @@ def auto_play(answer, verbose=False):
         game_chain += guess
         steps += 1
         if steps > 6:
-            verbose_print(verbose, "Failure!")
             break
     return steps
 
 
-def auto_play_hard_mode(answer, verbose=False):
-    verbose_print(verbose, f"{answer} -----")
-    steps = 1
-    game_list = answer_list
-    guess = 'arise'
-
-    while True:
-        result = checker(guess, answer)
-        game_list = reduce_by_guess_result(guess, result, game_list)
-        verbose_print(verbose, f"{guess} : {result} : {len(game_list)}")
-
-        if result == "22222":
+def start_word_outcome_step_total(start_word, limit=float('inf')):
+    step_total = 0
+    for answer_word in answer_list:
+        print(f"---{answer_word}")
+        current_steps = auto_play(answer_word, start_word)
+        if current_steps > 6:
+            step_total = float('inf')
             break
-        elif len(game_list) == 1:
-            guess = game_list[0]
+        step_total += current_steps
+        if step_total > limit:
+            step_total = float('inf')
+            break
+    return step_total
+
+
+def find_best_start(search_set):
+    best_word = 'xxxxx'
+    best_step_count = float('inf')
+
+    for start_word in search_set:
+        print(start_word)
+        total_steps_from_current_start_word = start_word_outcome_step_total(start_word, best_step_count)
+        if total_steps_from_current_start_word < best_step_count:
+            best_word = start_word
+            best_step_count = total_steps_from_current_start_word
+            print(f"New Best!: {best_word} with {best_step_count} steps")
         else:
-            guess, _ = most_reductive_word(game_list, game_list)
-        steps += 1
-        if steps > 6:
-            verbose_print(verbose, "Failure!")
-            break
-    return steps
+            print(f"Failed: {start_word}")
 
-
-# doesn't work too well
-def two_search():
-    smallest_largest = len(answer_list)
-    best_pair = ('xxxxx', 'yyyyy')
-    for word1, word2 in product(data_word_list, data_word_list):
-        game_list = answer_list
-        if word1 != word2:
-            largest = 0
-            for res1, res2 in product(possible_results, possible_results):
-                game_list = reduce_by_guess_result(word1, res1, game_list)
-                game_list = reduce_by_guess_result(word2, res2, game_list)
-                rem_size = len(game_list)
-                if rem_size > smallest_largest:
-                    largest = rem_size
-                    break
-                elif rem_size > largest:
-                    largest = rem_size
-            if largest < smallest_largest:
-                smallest_largest = largest
-                best_pair = (word1, word2)
-                print(best_pair, smallest_largest)
-    return best_pair
-
-
-# gives results, results aren't helpful
-def triple_search():
-    most_common = [i[0] for i in Counter(''.join(answer_list)).most_common(15)]
-    for p in product(answer_list, answer_list):
-        common_count = sum([mc in ''.join(p) for mc in most_common[:12]])
-        if common_count == 10:
-            cur_set = set(''.join(p))
-            com_set = set(most_common)
-            left_overs = list(com_set.difference(cur_set))
-            has_all_leftovers = list(filter(lambda ww: all([l_o in ww for l_o in left_overs]), answer_list))
-            for w in has_all_leftovers:
-                print(p[0], p[1], w)
+    return best_word, best_step_count
