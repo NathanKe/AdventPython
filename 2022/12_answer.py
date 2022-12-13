4c352a82f397114588d50ce18d3562605416ccf2
Nathan Kearney <nkearney89@gmail.com>
2022-12-12 19:45:42 -0500

    2022 day 12 - done!

--- 2022/12_answer.py
+++ 2022/12_answer.py
@@ -1,79 +1,64 @@
 from collections import deque
 from collections import defaultdict
+
 grid_lines = open('12_input').read().splitlines()
 
-
 grid = {}
-paths = []
 
-MAX_ROW = len(grid_lines) - 1
-MAX_COL = len(grid_lines[0]) - 1
+NUM_ROWS = len(grid_lines)
+NUM_COLS = len(grid_lines[0])
 
 end_loc = None
-
+start_loc = None
 
 for row_ix, row_val in enumerate(grid_lines):
     for col_ix, col_val in enumerate(list(row_val)):
         if col_val == 'S':
-            grid[complex(row_ix, col_ix)] = chr(ord('a') - 1)
-            paths.append([complex(row_ix, col_ix)])
+            grid[complex(row_ix, col_ix)] = chr(ord('a'))
+            start_loc = complex(row_ix, col_ix)
         elif col_val == 'E':
             end_loc = complex(row_ix, col_ix)
-            grid[end_loc] = chr(ord('z') + 1)
+            grid[end_loc] = chr(ord('z'))
         else:
             grid[complex(row_ix, col_ix)] = col_val
 
 
-def expand_path(in_path):
-    head = in_path[-1]
-    head_val = ord(grid[head])
-    north = head - 1
-    south = head + 1
-    east = head + 1j
-    west = head - 1j
-
-    return_paths = []
-
-    if north.real >= 0 and ord(grid[north]) - 1 <= head_val:
-        in_path_copy = in_path.copy()
-        in_path_copy.append(north)
-        return_paths.append(in_path_copy)
-        
-    if south.real <= MAX_ROW and ord(grid[south]) - 1 <= head_val:
-        in_path_copy = in_path.copy()
-        in_path_copy.append(south)
-        return_paths.append(in_path_copy)
-    
-    if east.imag <= MAX_COL and ord(grid[east]) - 1 <= head_val:
-        in_path_copy = in_path.copy()
-        in_path_copy.append(east)
-        return_paths.append(in_path_copy)
-    
-    if west.imag >= 0 and ord(grid[west]) - 1 <= head_val:
-        in_path_copy = in_path.copy()
-        in_path_copy.append(west)
-        return_paths.append(in_path_copy)
-
-    return return_paths
+def expand_frontier(in_frontier, in_steps_to_z_heights, in_min_distance_map):
+    new_frontier = []
+    while in_frontier:
+        cur_node = in_frontier.pop()
+        dist_to_cur = in_min_distance_map[cur_node]
+        neighbors = [cur_node + x for x in [1, 1j, -1, -1j]]
+        for neighbor in neighbors:
+            if 0 <= neighbor.real < NUM_ROWS and 0 <= neighbor.imag < NUM_COLS:
+                if ord(grid[neighbor]) == ord(grid[end_loc]):
+                    in_steps_to_z_heights.append(dist_to_cur + 1)
+                if ord(grid[neighbor]) - 1 <= ord(grid[cur_node]):
+                    if neighbor not in in_min_distance_map.keys():
+                        in_min_distance_map[neighbor] = dist_to_cur + 1
+                        new_frontier.append(neighbor)
+                    elif dist_to_cur + 1 < in_min_distance_map[neighbor]:
+                        in_min_distance_map[neighbor] = dist_to_cur + 1
+                        new_frontier.append(neighbor)
+    return new_frontier, in_steps_to_z_heights, in_min_distance_map
 
 
-min_dist_lookup = defaultdict(lambda: MAX_COL * MAX_ROW * 10000)
-
-complete_paths = []
-
-while paths:
-    active_path = paths.pop()
-    active_head = active_path[-1]
-    print(len(paths), grid[active_head], active_head, abs(active_head - end_loc))
-    if len(active_path) < min_dist_lookup[active_head]:
-        min_dist_lookup[active_head] = len(active_path)
-        if grid[active_head] == chr(ord('z') + 1):
-            complete_paths.append(active_path)
-        else:
-            expansion = expand_path(active_path)
-            for ex in expansion:
-                paths.append(ex)
-    paths.sort(key=lambda p: len(p), reverse=True)
+def shortest_length_path_to_height_z(in_start_loc):
+    frontier = [in_start_loc]
+    steps_to_z_heights = [10000]
+    min_distance_map = {in_start_loc: 0}
+    while frontier:
+        frontier, steps_to_z_heights, min_distance_map = expand_frontier(frontier, steps_to_z_heights, min_distance_map)
+    return min(steps_to_z_heights)
 
 
-print("Done!")
+print("Part 1:", shortest_length_path_to_height_z(start_loc))
+
+best_a_start = 10000
+for r in range(NUM_ROWS):
+    for c in range(NUM_COLS):
+        if grid[complex(r, c)] == 'a':
+            cur_a_start = shortest_length_path_to_height_z(complex(r, c))
+            if cur_a_start < best_a_start:
+                best_a_start = cur_a_start
+print("Part 2: ", best_a_start)
