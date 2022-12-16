656daf3ceb4c772787757a4f9bbe39bea5b041cb
Nathan Kearney <nkearney89@gmail.com>
2022-12-16 00:00:01 -0500

    2022 day 15

--- 2022/15_answer.py
+++ 2022/15_answer.py
@@ -10,92 +10,75 @@ for line in distance_report_lines:
     sensor_beacons.append((sx, sy, bx, by))
 
 
-SEARCH_ROW = 10
-
-
 def calc_manhattan_distance(jx, jy, kx, ky):
     return abs(jx - kx) + abs(jy - ky)
 
 
-left_bound = 0
-right_bound = 0
-for sx, sy, bx, by in sensor_beacons:
-
-    manhattan_distance = calc_manhattan_distance(sx, sy, bx, by)
-################ rebuilding for bounds checking version
-    # how on earth do you handle adding and merging disjoint ranges?
-    if sy - manhattan_distance <= SEARCH_ROW <= sy + manhattan_distance:
-        dist_to_search_row = abs(sy - SEARCH_ROW)
-        remaining_distance = manhattan_distance - dist_to_search_row
+def reduce_bound_pairs(pair_list):
+    pair_list.sort(key=lambda tu: tu[0])
+    if len(pair_list) == 1:
+        return pair_list
+    elif len(pair_list) == 2:
+        a_l, a_r = pair_list[0]
+        b_l, b_r = pair_list[1]
         # completely contained
-        # overlaps left side
-        # overlaps right side
+        if b_l <= a_l <= a_r <= b_r or a_l <= b_l <= b_r <= a_r:
+            return [(min(a_l, b_l), max(a_r, b_r))]
+        # overlaps
+        elif a_r >= b_l >= a_l or b_r >= a_l >= b_l:
+            return [(min(a_l, b_l), max(a_r, b_r))]
+        elif b_l <= a_r <= b_r or a_l <= b_r <= a_r:
+            return [(min(a_l, b_l), max(a_r, b_r))]
         # disjoint
+        else:
+            return pair_list
+    else:
+        # pairwise recursive reduction
+        cur_len = len(pair_list)
+        assert (cur_len >= 3)
+        pair_ix = 0
+        while True:
+            if pair_ix > cur_len - 2:
+                return pair_list
+            new_pairs = pair_list[:pair_ix] + reduce_bound_pairs(
+                [pair_list[pair_ix], pair_list[pair_ix + 1]]) + pair_list[pair_ix + 2:]
+            if len(new_pairs) == cur_len:
+                pair_ix += 1
+            else:
+                return reduce_bound_pairs(new_pairs)
 
 
-beacon_on_search_line_count = 0
-for sx, sy, bx, by in sensor_beacons:
-    if by == SEARCH_ROW:
-        beacon_on_search_line_count += 1
-
-print("Part 1: ", len(cant_be_beacon))
-
-
-# compute distance map
-# for each cell in search area
-# ---- for each beacon
-# --------- if in range
-# -------------- break
-# ----------
-
-
-sensor_distance_map = {}
-for sx, sy, bx, by in sensor_beacons:
-    sensor_distance_map[(sx, sy)] = calc_manhattan_distance(sx, sy, bx, by)
-
-
-def in_range_of_a_beacon(qx, qy):
+def positions_without_beacon(search_row, is_part_2):
+    bound_pairs = []
     for sx, sy, bx, by in sensor_beacons:
-        dist_to_beacon = calc_manhattan_distance(qx, qy, bx, by)
-        if dist_to_beacon <= sensor_distance_map[(sx, sy)]:
-            return True
-    return False
+        manhattan_distance = calc_manhattan_distance(sx, sy, bx, by)
+        if sy - manhattan_distance <= search_row <= sy + manhattan_distance:
+            dist_to_search_row = abs(sy - search_row)
+            remaining_distance = manhattan_distance - dist_to_search_row
+            cur_left_bound = sx - remaining_distance
+            cur_right_bound = sx + remaining_distance
+            if is_part_2:
+                if cur_left_bound <= 4000000 and cur_right_bound >= 0:
+                    bound_pairs.append((max(0, cur_left_bound), min(4000000, cur_right_bound)))
+            else:
+                bound_pairs.append((cur_left_bound, cur_right_bound))
+            bound_pairs = reduce_bound_pairs(bound_pairs)
+    return bound_pairs
 
 
-def set_builder(sx, sy, bx, by):
-    vis_set = []
-    m_d = calc_manhattan_distance(sx, sy, bx, by)
-    for vert_steps_taken in range(m_d + 1):
-        for steps_remain in range(m_d - vert_steps_taken + 1):
-            vis_set.append((sx - steps_remain, sy - vert_steps_taken))
-            vis_set.append((sx + steps_remain, sy - vert_steps_taken))
-            vis_set.append((sx - steps_remain, sy + vert_steps_taken))
-            vis_set.append((sx + steps_remain, sy + vert_steps_taken))
-    return vis_set
+p1_res = positions_without_beacon(2000000, False)
+assert (len(p1_res) == 1)
+# lucked out here... off by one error in the range width calculation
+# off by one error in positions on line 2000000 that are THEMSELVES beacons
+print("Part 1: ", abs(p1_res[0][1] - p1_res[0][0]))
 
+for i in range(0, 4000001):
+    x = positions_without_beacon(i, True)
+    if len(x) == 1 and x[0] == (0, 4000000):
+        pass
+    else:
+        print("Part 2: ", (x[0][1] + 1) * 4000000 + i)
+        break
 
-def one_outside_of_range(sx, sy, bx, by):
-    m_d = calc_manhattan_distance(sx, sy, bx, by)
-
-    shell = []
-    for i in range(0, m_d + 1):
-        shell.append((sx + i + 1, sy + m_d - i))
-        shell.append((sx + i + 1, sy - m_d + i))
-        shell.append((sx - i + 1, sy + m_d - i))
-        shell.append((sx - i + 1, sy - m_d + i))
-
-    return shell
-
-# mega_set = set().union(*[set_builder(*sb) for sb in sensor_beacons])
-print("Done!")
-
-
-
-# current thought:  calculate 'shells' of each sensor - those spaces exactly one unit out of range
-# iterate over those, stopping when you find one that is out of range of
-# shell size is square of (range + 1)
-
-
-# pair-wise intersect "shells"?
-
-# work more with range limits, not sets/arrays of actual ranges
+# the right answer is clever vector intersection math of the "shell" one step outside each scanners range
+#
