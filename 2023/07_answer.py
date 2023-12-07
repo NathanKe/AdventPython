a9d7b00ca6cfabcd77660454f8812b1772d73921
NathanKe <nkearney89@gmail.com>
2023-12-07 15:25:24 -0500

    2023 - day 7 - p1 & p2

--- 2023/07_answer.py
+++ 2023/07_answer.py
@@ -40,7 +40,6 @@ def hand_comparator(hand_tuple_a, hand_tuple_b):
     counts_a.sort(reverse=True)
     counts_b.sort(reverse=True)
 
-
     if counts_a < counts_b:
         return -1
     elif counts_a > counts_b:
@@ -61,3 +60,69 @@ winnings = sum([(h[0]+1) * h[1][1] for h in enumerate(hands)])
 print(winnings)
 
 
+def highest_most_common_non_joker(i_hand):
+    non_joker = [card for card in i_hand if card != 11]
+    hand_counter = co(non_joker)
+    most_common_count = hand_counter.most_common()[0][1]
+    equally_most_common = [tu for tu in hand_counter.items() if tu[1] == most_common_count]
+    highest_most_common = sorted([tu[0] for tu in equally_most_common])[-1]
+    return highest_most_common
+
+
+def best_use_of_joker(i_hand):
+    if i_hand == [11, 11, 11, 11, 11]:
+        return [14, 14, 14, 14, 14]
+    else:
+        target = highest_most_common_non_joker(i_hand)
+        new_hand = []
+        for card in i_hand:
+            if card == 11:
+                new_hand.append(target)
+            else:
+                new_hand.append(card)
+    return new_hand
+
+
+def low_val_joker(i_hand):
+    new_hand = []
+    for card in i_hand:
+        if card == 11:
+            new_hand.append(0)
+        else:
+            new_hand.append(card)
+    return new_hand
+
+
+def hand_comparator_2(hand_tuple_a, hand_tuple_b):
+    hand_a = hand_tuple_a[0]
+    hand_b = hand_tuple_b[0]
+    joker_a = best_use_of_joker(hand_a)
+    joker_b = best_use_of_joker(hand_b)
+    cnt_a = co(joker_a)
+    cnt_b = co(joker_b)
+    counts_a = pad_zeros(list(cnt_a.values()), 5)
+    counts_b = pad_zeros(list(cnt_b.values()), 5)
+    counts_a.sort(reverse=True)
+    counts_b.sort(reverse=True)
+
+    if counts_a < counts_b:
+        return -1
+    elif counts_a > counts_b:
+        return 1
+    else:
+        low_joker_a = low_val_joker(hand_a)
+        low_joker_b = low_val_joker(hand_b)
+
+        if low_joker_a < low_joker_b:
+            return -1
+        elif low_joker_a > low_joker_b:
+            return 1
+        else:
+            return 0
+
+
+hands.sort(key=functools.cmp_to_key(hand_comparator_2))
+
+winnings2 = sum([(h[0]+1) * h[1][1] for h in enumerate(hands)])
+
+print(winnings2)
