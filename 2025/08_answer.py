dad609b90d6c8bf472548c8a91e5e31755f2ec34
NathanKe <nkearney89@gmail.com>
2025-12-11 16:15:50 -0500

    2025 d8p1

--- 2025/08_answer.py
+++ 2025/08_answer.py
@@ -20,35 +20,73 @@ junction_pairs.sort(key=lambda p: distance(*p))
 
 circuits = []
 
-for i in range(10):
+for i in range(1000):
     cur_pair = junction_pairs[i]
-    print(cur_pair)
-    print(circuits)
     found = False
     for circ in circuits:
         if not found:
             left_in = cur_pair[0] in circ
             right_in = cur_pair[1] in circ
             if left_in and right_in:
-                print("both")
                 found = True
             elif left_in:
-                print("left")
                 circ.append(cur_pair[1])
                 found = True
             elif right_in:
-                print("right")
                 circ.append(cur_pair[0])
                 found = True
     if not found:
-        print("neither")
         circuits.append(list(cur_pair))
-    print(circuits)
-    print("-----------------")
 
+
+def redux(i_circs):
+    while True:
+        index_pairs = list(combinations(range(len(i_circs)), 2))
+        new_circuits = i_circs[::]
+        for ip in index_pairs:
+            m_a = i_circs[ip[0]]
+            m_b = i_circs[ip[1]]
+            if set(m_a).intersection(set(m_b)):
+                new_circuits = []
+                left_alone = list(set(range(len(i_circs))).difference({ip[0], ip[1]}))
+                mmm = list(set(m_a).union(set(m_b)))
+                new_circuits.append(mmm)
+                for la in left_alone:
+                    new_circuits.append(i_circs[la])
+                break
+        if i_circs == new_circuits:
+            break
+        else:
+            i_circs = new_circuits[::]
+    return i_circs
+
+
+
+circuits = redux(circuits)
 
 
 circ_lens = list(map(len, circuits))
 circ_lens.sort(reverse=True)
 print(circ_lens[0] * circ_lens[1] * circ_lens[2])
+
+
+for jp in junction_pairs:
+    cur_pair = jp
+    found = False
+    for circ in circuits:
+        if not found:
+            left_in = cur_pair[0] in circ
+            right_in = cur_pair[1] in circ
+            if left_in and right_in:
+                found = True
+            elif left_in:
+                circ.append(cur_pair[1])
+                found = True
+            elif right_in:
+                circ.append(cur_pair[0])
+                found = True
+    if not found:
+        circuits.append(list(cur_pair))
+
+
 
